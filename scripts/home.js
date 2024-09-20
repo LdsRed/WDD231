4fda1ee68deb732e7ab533e1c3016ae3a41e3c68
Jordan Larcher <lds.larcher@gmail.com>
2024-09-20 15:46:19 -0300

    updating hamburger menu

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,9 @@
 document.getElementById('menu-toggle').addEventListener('click', () => {
    const menu = document.getElementById('main-menu');  
-   if (menu.style.display === 'block') {
+   if (menu.style.display === 'flex') {
         menu.style.display = 'none'; 
    }else{
-    menu.style.display = 'block';
+    menu.style.display = 'flex';
    }
 });
 
