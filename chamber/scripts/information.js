a0675426b5401fad1b7d5a727291695de114a162
jordan larcher <lds.larcher@gmail.com>
2024-10-19 21:12:23 -0300

    updating discovery page

--- chamber/scripts/information.js
+++ chamber/scripts/information.js
@@ -1,5 +1,4 @@
 const information = document.querySelector("#visit-message");
-const today = Date.now();
 
 const lastVisit = localStorage.getItem('lastVisit');
 
@@ -7,10 +6,24 @@ const lastVisit = localStorage.getItem('lastVisit');
 
 function displayLastVisit() {
     const lastVisit = localStorage.getItem('lastVisit');
+    let today = Date.now();
+    const milisecondsInDay = 24*60*60*1000;
 
     if(lastVisit == null) {
-        information.innerText = "No last Visit";
-        console.log("NO LAST VISIT");
+        information.innerText = `Welcome! Let us know if you have any questions.`;
+    }else {
+        let timeSinceLastVisit = today - parseInt(lastVisit);
+        if(timeSinceLastVisit < milisecondsInDay) {
+            information.innerText = `Back so soon! Awesome!`;
+        }else {
+            let diffDays = Math.round(timeSinceLastVisit / milisecondsInDay);
+
+            if(diffDays === 1){
+                information.innerText = `You last visited ${diffDays} day ago.`;
+            }else {
+                information.innerText=`You last visited ${diffDays} days ago.`;
+            }
+        }
     }
 }
 
